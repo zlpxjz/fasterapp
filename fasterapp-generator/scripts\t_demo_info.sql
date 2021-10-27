a6e2672fbb9f9e2033f2652bd2b94f831be1af92
zlpxjz <914766813@qq.com>
2021-10-27 13:07:21 +0800

    Delete scripts\t_demo_info.sql

--- fasterapp-generator/scripts\t_demo_info.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS t_demo_info;
-
-CREATE TABLE t_demo_info (
-    name   varchar(32),
-    id   Integer(11) not null,
-    PRIMARY KEY (,id)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
